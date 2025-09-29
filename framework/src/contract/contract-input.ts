16799b7d58b100d616ff6300d9371de461960c55
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2025-09-28 22:31:22 -0700

    Formatting changes on ContractInput

--- framework/src/contract/contract-input.ts
+++ framework/src/contract/contract-input.ts
@@ -5,11 +5,7 @@ import { ContractObject } from './contract-object';
 export class ContractInput extends ContractObject {
   public static parse<T extends typeof ContractObject>(this: T, unknown: unknown, errorConstructor: Utils.Constructor<Error, [string]> = Error): z.infer<T['Schema']> {
     const { data, success, error } =  this.Schema.safeParse(unknown)
-
-    if(success) {
-      return data as z.infer<T['Schema']>
-    }
-
-    throw new errorConstructor(error.issues?.[0]?.message)
+    if(!success) throw new errorConstructor(error.issues?.[0]?.message)
+    return data as z.infer<T['Schema']>
   }
 }
